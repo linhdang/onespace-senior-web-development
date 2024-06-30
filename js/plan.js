6e06d862e9133e8cef4886a27bee2451ce8d5086
Linh Dang <linh.danghong@gmail.com>
2024-06-30 14:12:25 +0100

    Create plan dynamically

---
+++ js/plan.js
@@ -0,0 +1,38 @@
+(function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        const data = [
+                { id: 1, goal: 'Learn Web Development on Node.js', date: '06/10/2024' },
+                { id: 2, goal: 'Win CodeQuest 2024', date: '06/04/2024' },
+                { id: 3, goal: 'Win CodeQuest 2025', date: '06/04/2025' },
+                { id: 4, goal: 'Win CodeQuest 2026', date: '06/04/2026' },
+                { id: 5, goal: 'Win CodeQuest 2027', date: '06/04/2027' },
+                { id: 6, goal: 'Win CodeQuest 2028', date: '06/04/2028' },
+                { id: 7, goal: 'Win CodeQuest 2029', date: '06/04/2029' }
+            ];
+        const table = document.getElementById('plan');
+
+            const tbody = document.createElement('tbody');
+
+            data.forEach(item => {
+                const row = document.createElement('tr');
+
+                const idCell = document.createElement('th');
+                idCell.scope = 'row';
+                idCell.textContent = item.id;
+                row.appendChild(idCell);
+
+                const goalCell = document.createElement('td');
+                goalCell.textContent = item.goal;
+                row.appendChild(goalCell);
+
+                const dateCell = document.createElement('td');
+                dateCell.textContent = item.date;
+                row.appendChild(dateCell);
+
+                tbody.appendChild(row);
+            });
+
+            table.appendChild(tbody);
+      }, false);
+})();
