6e6b7099793d413ef2373a51b4942e95d59621e2
Linh Dang <linh.danghong@gmail.com>
2024-10-11 06:26:22 +0100

    Revert "lesson 9 course 3"
    This reverts commit e3087f08eaf25162c5680f0e76a61261b6318382.

--- server/server.js
+++ server/server.js
@@ -9,32 +9,6 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     // Send the response text
     res.end('Hello, this is your GET response from Node.js server!');
-  } else if (req.url == "/data/menus") {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end('{' +
-                 '"menuItems": [' +
-                 '  { "name": "Home", "link": "index.html" },' +
-                 '  { "name": "Achievement", "link": "achievement.html" }, ' +
-                 '  { "name": "Contact", "link": "contact.html" }' +
-               ' ]'+
-               '}'
-               );
-      return;
-
-  } else if (req.url == "/data/plans") {
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          res.end('[ ' +
-                     '{ "id": 1, "goal": "Learn Web Development on Node.js", "date": "06/10/2024" },' +
-                     '{ "id": 2, "goal": "Win CodeQuest 2024", "date": "06/04/2024" },' +
-                     '{ "id": 3, "goal": "Win CodeQuest 2025", "date": "06/04/2025" },' +
-                     '{ "id": 4, "goal": "Win CodeQuest 2026", "date": "06/04/2026" },' +
-                     '{ "id": 5, "goal": "Win CodeQuest 2027", "date": "06/04/2027" },' +
-                     '{ "id": 6, "goal": "Win CodeQuest 2028", "date": "06/04/2028" },' +
-                     '{ "id": 7, "goal": "Win CodeQuest 2029", "date": "06/04/2029" }' +
-                   ']'
-                   );
-          return;
-
   } else {
     // Handle 404 for other routes
     res.writeHead(404, { 'Content-Type': 'text/plain' });
